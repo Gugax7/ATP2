0f53fcdba7e0eb62ea7f6d95edc613e3ca94a2e8
Gustavo Salmazo <gux.salmazo@gmail.com>
2024-11-08 11:30:54 -0300

    Trying to understand GPT answer

--- Pre-Exam/exercise7.c
+++ Pre-Exam/exercise7.c
@@ -7,36 +7,84 @@ typedef struct{
     struct Aluno *p;
 }Aluno;
 
-void selection_sort(Aluno *L){
-    int size = 0;
-    Aluno *lower_index = L;
-    Aluno *aux1 = L;
+/*void selection_sort(Aluno **L){
+    Aluno *lower_index;
+    Aluno *aux1 = *L;
     Aluno *aux2;
     Aluno *who_points_to_lower;
     Aluno *who_points_to_aux2;
-    aux2 = L;
-    while(aux2 != NULL){
-        aux2 = aux2->p;
-        size++;
-    }
-    aux2 = L;
     
-    int last_mat = aux2->matricula;
     while(aux1 != NULL){
         aux2 = aux1;
         lower_index = aux1;
+        printf("\ncurrent aux1: %d", aux1->matricula);
         while(aux2 != NULL){
             who_points_to_aux2 = aux2;
             aux2 = aux2->p;
-            if(aux2->matricula > lower_index->matricula){
+            printf("\ncurrent aux2: %d", aux2->matricula);
+            if(aux2->matricula < lower_index->matricula){
                 lower_index = aux2;
                 who_points_to_lower = who_points_to_aux2;
+                printf("\nwho points to lower: %d", who_points_to_lower->matricula);
+                printf("\ncurrent lower: %d", lower_index->matricula);
             }
         }
+        
+        printf("\nwho points to lower: %d", who_points_to_lower->matricula);
+        printf("\naux1: %d", aux1->matricula);
         who_points_to_lower->p = lower_index->p;
-        lower_index->p = aux1->p;
-        aux1->p = lower_index;
-        aux1 = aux1->p;
+        lower_index->p = aux1;
+        if(lower_index == aux1){
+            aux1 = aux1->p;
+        }
+        if(aux1 == *L){
+            *L = lower_index;
+        }
+        printf("points changed");
+    }
+}*/
+
+void selection_sort(Aluno **L) {
+    if (L == NULL || *L == NULL) return; // Check for an empty list
+
+    Aluno *aux1 = *L;
+    Aluno *prev_aux1 = NULL;
+
+    while (aux1 != NULL && aux1->p != NULL) {
+        Aluno *lower_index = aux1;
+        Aluno *prev_lower = prev_aux1;
+        Aluno *aux2 = aux1->p;
+        Aluno *prev_aux2 = aux1;
+
+        // Find the minimum node in the unsorted portion of the list
+        while (aux2 != NULL) {
+            if (aux2->matricula < lower_index->matricula) {
+                lower_index = aux2;
+                prev_lower = prev_aux2;
+            }
+            prev_aux2 = aux2;
+            aux2 = aux2->p;
+        }
+
+        // If the minimum node is not the current node, swap them
+        if (lower_index != aux1) {
+            if (prev_aux1 != NULL) {
+                prev_aux1->p = lower_index;
+            } else {
+                *L = lower_index;
+            }
+
+            prev_lower->p = aux1;
+
+            // Swap the next pointers
+            Aluno *temp = lower_index->p;
+            lower_index->p = aux1->p;
+            aux1->p = temp;
+        }
+
+        // Move `aux1` and `prev_aux1` forward
+        prev_aux1 = lower_index;
+        aux1 = lower_index->p;
     }
 }
 
@@ -98,8 +146,8 @@ int main(){
             insereFinal(&L,mat,nota);
         }
     }while(mat >0);
-    insereAfter(L,4,6,3);
-    selection_sort(L);
+    //insereAfter(L,4,6,3);
+    selection_sort(&L);
     print_list(L);
     return 0;
 }
